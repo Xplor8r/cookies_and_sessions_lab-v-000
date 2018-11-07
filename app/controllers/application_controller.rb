f0666d9928b9a2ea9270bd4cab96789b790f1558
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-07 03:25:20 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
   helper_method :cart
 
   def cart
-    cart = session[:cart] || cart = []
-    cart
+    session[:cart] = session[:cart] || []
+
   end
 
 end
