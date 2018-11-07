ff1f2560acd8254be31da6e26db57cf4a3639327
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-07 03:15:28 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root 'products#index'
 
   resources :products
-
+  root 'products#index'
+  post '/' => 'products#add'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
