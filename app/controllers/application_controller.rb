96175c94deb5a55ccf2f5bdcaeb91796ae032242
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-07 02:53:09 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def cart
-    @cart = session[:cart_id]
+    @cart = session[:cart_id] || []
   end
 
 end
